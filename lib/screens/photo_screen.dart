96182b066c9a1946b5f3e3cd2d7dca601d895b25
Vuffi2007 <95097824+Vuffi2007@users.noreply.github.com>
2025-09-15 12:46:21 +0200

    start sketch

---
+++ lib/screens/photo_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class PhotoScreen extends StatelessWidget {
+  const PhotoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Photo Screen', style: TextStyle(fontSize: 24)),
+    );
+  }
+}
