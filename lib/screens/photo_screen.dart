ba09a75b2d5b76b042384fbf1c272e21c8948fd8
Vuffi2007 <95097824+Vuffi2007@users.noreply.github.com>
2025-09-24 18:48:05 +0200

    Basic form to add fish

--- lib/screens/photo_screen.dart
+++ lib/screens/photo_screen.dart
@@ -3,6 +3,9 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:wild_note/colors.dart';
 
 class PhotoScreen extends StatefulWidget {
   const PhotoScreen({super.key, required this.camera});
@@ -20,10 +23,7 @@ class PhotoScreenState extends State<PhotoScreen> {
   @override
   void initState() {
     super.initState();
-    _controller = CameraController(
-      widget.camera,
-      ResolutionPreset.medium,
-    );
+    _controller = CameraController(widget.camera, ResolutionPreset.medium);
 
     _initializeControllerFuture = _controller.initialize();
   }
@@ -38,6 +38,7 @@ class PhotoScreenState extends State<PhotoScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Take a picture')),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
       body: FutureBuilder<void>(
         future: _initializeControllerFuture,
         builder: (context, snapshot) {
@@ -59,9 +60,8 @@ class PhotoScreenState extends State<PhotoScreen> {
 
             await Navigator.of(context).push(
               MaterialPageRoute<void>(
-                builder: (context) => DisplayPictureScreen(
-                  imagePath: image.path,
-                ),
+                builder: (context) =>
+                    DisplayPictureScreen(imagePath: image.path),
               ),
             );
           } catch (e) {
@@ -74,16 +74,92 @@ class PhotoScreenState extends State<PhotoScreen> {
   }
 }
 
-class DisplayPictureScreen extends StatelessWidget {
+class DisplayPictureScreen extends StatefulWidget {
   final String imagePath;
 
   const DisplayPictureScreen({super.key, required this.imagePath});
 
   @override
+  State<DisplayPictureScreen> createState() => _DisplayPictureScreenState();
+}
+
+class _DisplayPictureScreenState extends State<DisplayPictureScreen> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _lengthController = TextEditingController();
+  final TextEditingController _weightController = TextEditingController();
+
+  int count = 0;
+
+  @override
+  void dispose() {
+    _lengthController.dispose();
+    _weightController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Display the Picture')),
-      body: Image.file(File(imagePath)),
+      appBar: AppBar(title: const Text('Is this picture good?')),
+      body: SingleChildScrollView(
+        // prevents overflow when keyboard opens
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                Image.file(File(widget.imagePath)),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _lengthController,
+                  decoration: const InputDecoration(
+                    labelText: 'Length of fish',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter something';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+                TextFormField(
+                  controller: _weightController,
+                  decoration: const InputDecoration(
+                    labelText: 'Weight of fish',
+                    border: OutlineInputBorder(),
+                  ),
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  validator: (value) {
+                    if (value == null || value.isEmpty) {
+                      return 'Please enter something';
+                    }
+                    return null;
+                  },
+                ),
+                const SizedBox(height: 16),
+              ],
+            ),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          if (_formKey.currentState!.validate()) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text('Saved fish!'), backgroundColor: blueFishColorScheme.secondary),
+            );
+          }
+        },
+        child: const Icon(Icons.check),
+      ),
     );
   }
 }
